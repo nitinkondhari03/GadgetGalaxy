8eaa71b4566268b678d49ac19808e66271aaf661
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-01-31 20:11:10 +0530

    git add

--- frontend/dailyobject/src/components/Navbar/Navbar.jsx
+++ frontend/dailyobject/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./navbar.css";
+import "./Navbar.css";
 import Logo from "./Gadget.png";
 import { Link, useNavigate } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
